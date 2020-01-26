276645e36e5c090c4953ddd432cbb90e3d90e28f
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-01-26 16:35:32 -0600

    added methods push, pop, clear to API with query params

--- bankAPI/src/main/java/com/bank/bankAPI/Bank.java
+++ bankAPI/src/main/java/com/bank/bankAPI/Bank.java
@@ -1,30 +1,68 @@
 package com.bank.bankAPI;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@RestController
 public class Bank {
 
-    static void pop(ArrayList<String> bankQueue) {
+    public static String itemRemoved(){
+        String itemDeleted =  BankApiApplication.bankQueue.get(0);
+        return itemDeleted;
+    }
+
+    public static ArrayList<String> pop(ArrayList<String> bankQueue) {
         String itemRemoved = bankQueue.get(0);
 
         bankQueue.remove(0);
 
         System.out.println("The item " + itemRemoved + " was removed from the array " + bankQueue);
 
+        return bankQueue;
+
     }
 
-    static void push(ArrayList<String> bankQueue, String item) {
+    public static ArrayList<String> push(ArrayList<String> bankQueue, String item) {
         bankQueue.add(item);
         System.out.println("The item " + item + " was added to the array " + bankQueue);
+        return bankQueue;
 
     }
 
-    static void clear(ArrayList<String> bankQueue) {
+    public static ArrayList<String>  clear(ArrayList<String> bankQueue) {
         bankQueue.clear();
         System.out.println("The array was cleared " + bankQueue);
 
+        return bankQueue;
+
+    }
+
+    @GetMapping("/push")
+    @ResponseBody
+    public String getFoos(@RequestParam String amount) {
+        push(BankApiApplication.bankQueue, amount);
+        return "Array: " + BankApiApplication.bankQueue + "\nItem pushed: " + amount;
+    }
+
+    @GetMapping("/pop")
+    @ResponseBody
+    public String getFaas() {
+        String data = itemRemoved();
+        pop(BankApiApplication.bankQueue);
+        return "Array: " + BankApiApplication.bankQueue + "\nItem removed: " + data ;
+    }
+
+    @GetMapping("/clear")
+    @ResponseBody
+    public String getFuus() {
+        clear(BankApiApplication.bankQueue);
+        return "The array was cleared : " + BankApiApplication.bankQueue;
     }
 
 
--- bankAPI/src/main/java/com/bank/bankAPI/BankApiApplication.java
+++ bankAPI/src/main/java/com/bank/bankAPI/BankApiApplication.java
@@ -3,8 +3,13 @@ package com.bank.bankAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class BankApiApplication {
+
+	public static ArrayList<String> bankQueue = new ArrayList<>(Arrays.asList("deposit_100", "withdraw_300", "withdraw_243"));
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApiApplication.class, args);
