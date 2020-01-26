9c30c63fbef2dd59e74e5a87dbf590dc3c077c88
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-01-26 06:41:11 -0600

    created methods for push, pop, run

--- bankAPI/src/main/java/com/bank/bankAPI/Bank.java
+++ bankAPI/src/main/java/com/bank/bankAPI/Bank.java
@@ -6,15 +6,37 @@ import java.util.Arrays;
 
 public class Bank {
 
+    static void pop(ArrayList<String> bankQueue) {
+        String itemRemoved = bankQueue.get(0);
+
+        bankQueue.remove(0);
+
+        System.out.println("The item " + itemRemoved + " was removed from the array " + bankQueue);
+
+    }
+
+    static void push(ArrayList<String> bankQueue, String item) {
+        bankQueue.add(item);
+        System.out.println("The item " + item + " was added to the array " + bankQueue);
+
+    }
+
+    static void clear(ArrayList<String> bankQueue) {
+        bankQueue.clear();
+        System.out.println("The array was cleared " + bankQueue);
+
+    }
+
+
+
     public static void main(String[] args){
         ArrayList<String> bankQueue = new ArrayList<>(Arrays.asList("deposit_100", "withdraw_300", "withdraw_243"));
-        bankQueue.add("deposit_123");
         System.out.println(bankQueue);
-
-        int lastElement = bankQueue.size() - 1;
-        bankQueue.remove(lastElement);
-        System.out.println(bankQueue);
+        push(bankQueue, "deposit_1654");
+        pop(bankQueue);
+        clear(bankQueue);
     }
+
 
 }
 
