a272f7732a8744353a9a474e3b9c7383f6be420d
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-01-29 09:00:29 -0600

    created debugging demo

---
+++ debuggin_demo/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static int add( int x , int y){
+        int z =8;
+        int result = z * (x+ y);
+        return result;
+    }
+
+    public static void main(String[]args){
+
+        int n1 = 4;
+        int n2 = 5;
+        int result = add(n1, n2);
+        System.out.println(result);
+    }
+
+}
+
+
