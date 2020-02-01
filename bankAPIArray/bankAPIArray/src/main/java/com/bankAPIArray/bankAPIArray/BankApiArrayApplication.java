a30d23be2122ec4e63645e50c0c8cf0c62b598d4
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-02-01 12:10:55 -0600

    created spring API for bank array

---
+++ bankAPIArray/bankAPIArray/src/main/java/com/bankAPIArray/bankAPIArray/BankApiArrayApplication.java
@@ -0,0 +1,13 @@
+package com.bankAPIArray.bankAPIArray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiArrayApplication.class, args);
+	}
+
+}
---
+++ bankAPIArray/bankAPIArray/src/test/java/com/bankAPIArray/bankAPIArray/BankApiArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankAPIArray.bankAPIArray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiArrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
