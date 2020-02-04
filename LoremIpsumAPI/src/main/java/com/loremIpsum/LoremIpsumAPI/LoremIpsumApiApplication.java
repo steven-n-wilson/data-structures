2c92afb7becc2b965f86beaf614cd32080891c65
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-02-04 07:41:23 -0600

    created Lorem Ipsum API

---
+++ LoremIpsumAPI/src/main/java/com/loremIpsum/LoremIpsumAPI/LoremIpsumApiApplication.java
@@ -0,0 +1,13 @@
+package com.loremIpsum.LoremIpsumAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremIpsumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremIpsumApiApplication.class, args);
+	}
+
+}
---
+++ LoremIpsumAPI/src/test/java/com/loremIpsum/LoremIpsumAPI/LoremIpsumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loremIpsum.LoremIpsumAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoremIpsumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
